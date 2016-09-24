48a541ca79aa56fc26bfa11037988faf8bb3a1c0
jonti09 <jontisavani@gmail.com>
2016-09-24 13:42:11 +0530

    Added yesterday url

--- Notification/urls.py
+++ Notification/urls.py
@@ -2,15 +2,22 @@ from django.conf.urls import url
 from django.views.generic import ListView
 from Notification.models import ToiNews, Weather, Help
 from Notification.views import sleep
+from datetime import datetime, timedelta
+
 
 urlpatterns = [
-    url(r'^weather/',
-        ListView.as_view(queryset=Weather.objects.all().order_by('-date')[:1], template_name='weather.html')),
 
     url(r'^$',
         ListView.as_view(queryset=ToiNews.objects.all().order_by('-date')[:10], template_name='notification.html')),
 
-    url(r'^help', ListView.as_view(queryset=Help.objects.all(), template_name='help.html')),
+    url(r'^yesterday/$',
+        ListView.as_view(queryset=ToiNews.objects.filter(date__lte=(datetime.now() - timedelta(days=1)))[:10],
+                         template_name='news_pre.html', )),
+
+    url(r'weather/',
+        ListView.as_view(queryset=Weather.objects.all().order_by('-date')[:1], template_name='weather.html')),
+
+    url(r'help', ListView.as_view(queryset=Help.objects.all(), template_name='help.html')),
 
     url(r'^sleep', sleep),
 ]
