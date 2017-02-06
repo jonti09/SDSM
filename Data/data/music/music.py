ec40442736e6caf83ecb3c227946ffa8cd4992a9
jonti09 <jontisavani@gmail.com>
2017-02-06 17:25:05 +0530

    Added Music

---
+++ Data/data/music/music.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import pygame
+import os
+
+
+def get_songs():
+    print(os.getcwd())
+    if os.path.exists('/home/viper/PycharmProjects/project/Data/data/music/songs'):
+        os.chdir('/home/viper/PycharmProjects/project/Data/data/music/songs/')
+    else:
+        exit(0)
+
+    songs = []
+    for subdir, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.mp3' or '.wav' or '.ogg' or '.wma' or '.aac'):
+                songs.append(file)
+
+    return songs[:10]
+
+
+def play_song(cmd, num=0):
+    pygame.init()
+    songs = get_songs()
+
+    if cmd == "play":
+        pygame.mixer.music.load(songs[num])
+        pygame.mixer.music.play(-1, 0.0)
+
+    if cmd == "next":
+        num += 1
+        if num == len(songs):
+            num = 0
+
+        pygame.quit()
+        pygame.init()
+        pygame.mixer.music.load(get_songs()[num])
+        pygame.mixer.music.play(-1, 0.0)
+
+    elif cmd == "prev":
+        num -= 1
+        if num == -1:
+            num = len(songs)
+        pygame.quit()
+        pygame.init()
+        pygame.mixer.music.load(get_songs()[num])
+        pygame.mixer.music.play(-1, 0.0)
+
+    elif cmd == "pause":
+        pygame.mixer.music.pause()
+
+    elif cmd == "resume":
+        pygame.mixer.music.unpause()
+
+    elif cmd == "last":
+        num = len(songs) - 1
+        pygame.quit()
+        pygame.init()
+        pygame.mixer.music.load(get_songs()[num])
+        pygame.mixer.music.play(-1, 0.0)
+
+    elif cmd == "stop":
+        pygame.quit()
+        exit(0)
